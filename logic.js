f6313580e051ea481830ebda30b380ec8d82fd2a
MATRUNI <h.uyadav2004@gmail.com>
2025-11-26 14:32:56 +0530

    modification for handling the path on the frondend side

--- logic.js
+++ logic.js
@@ -9,28 +9,27 @@ let dirArray;
 let pwd=["home"]
 let pushStack=[];
 const head=document.getElementById("head");
-callingAPI(pwd)
+callingAPI(pwd);
 
 
 display.addEventListener('click', item=>{
-    pwd.push(item.target.textContent);
-    // pushStack.push(item.target.textContent);
-    fetcher(item.target.textContent, true);
+    pwd.push(pwd[pwd.length-1]+"/"+item.target.textContent);
+    fetcher(pwd[pwd.length-1], true);
 });
 
 head.addEventListener("click", e=>{
-    const child=e.target.closest("div");
-    console.log(Array.from(head.children).indexOf(child));
-    if(Array.from(head.children).indexOf(e.target.closest("div"))===0)
+    const child=Array.from(head.children).indexOf(e.target.closest("div"));
+    console.log(child);
+    if(child===0&&pwd.length>=2)
     {
         console.log("If condtion here")
-        let x=pwd.pop();
+        pushStack.push(pwd.pop());
         console.log("pwd:",pwd);
-        pushStack.push(x)
         console.log("pushStack",pushStack);
-        fetcher(x,false)
+        // console.log("Sending False here")
+        fetcher(pwd[pwd.length-1],false);
     }
-    else if(pushStack.length!==0&&Array.from(head.children).indexOf(e.target.closest("div"))===1)
+    else if(pushStack.length!==0&&child===1)
     {
         console.log("Else if condition here")
         let x=pushStack.pop();
@@ -39,30 +38,35 @@ head.addEventListener("click", e=>{
         console.log("pushStack",pushStack)
         fetcher(x, true);
     }
+    limitStack(pushStack);
 })
 
 
 function fetcher(a, order)
 {
-    fetch("http://localhost:3000",{
+    // console.log("Got here:", order,"&&&",a)
+    fetch("http://localhost:3000/navigate",{
         method: "POST",
         headers: {"Content-Type":"application/json"},
         body: JSON.stringify({body:a, forward:order})
     })
     .then(res=>res.json())
     .then(result=>{
-        // console.log(result);
-        getCall()
+        //this one line took me hours to fix lol, why? because i put the get call function in .then directly, it was executing synchronously
+        getCall();
     }).catch(err=> console.log(err));
 }
 function getCall()
 {
     const xhr=new XMLHttpRequest();
 
-    xhr.open("GET", "http://localhost:3000")
+    xhr.open("GET", "http://localhost:3000/to")
 
     xhr.onload=()=>{
-        createDivs(JSON.parse(xhr.responseText))
+        dirArray=JSON.parse(xhr.responseText);
+        // console.log(dirArray.body);
+        // console.log("Call createDivs function!");
+        createDivs(dirArray.body);
     }
     xhr.send();
 }
@@ -72,13 +76,18 @@ function callingAPI(s)
     xml.open("GET", `http://localhost:3000/${s.join("/")}`)
     xml.onload=()=>{
         dirArray=JSON.parse(xml.responseText);
-
-        createDivs(dirArray);
+        pwd.pop();
+        pwd.push(dirArray.folder);
+        createDivs(dirArray.body);
     }
     
     xml.send();
 }
 
+function limitStack(stack)
+{
+    return stack.length<=10?stack:stack.shift();
+}
 
 function createDivs(arr)
 {
